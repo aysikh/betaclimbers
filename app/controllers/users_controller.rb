b2dc62cb31e283facf9555b89a2b537718afd209
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-28 16:57:37 -0400

    Need to debug my incorrect username from login path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,19 @@ class UsersController < ApplicationController
   end
 
   def login
+    
+  end
+
+  def process_login
+    user = User.find_by(username: params[:username])
+    if user
+      redirect_to climbers_path
+    else
+      flash.now[:no_user] = "That user does not exist"
+      render :locations_path
+    end
+
+
   end
 
   def logout
