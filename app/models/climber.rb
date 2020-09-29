e3b593e00d727b41d0fdc1419f55b6bdcb304022
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-29 16:40:00 -0400

    Validations working on create but not login

--- app/models/climber.rb
+++ app/models/climber.rb
@@ -6,16 +6,6 @@ class Climber < ApplicationRecord
   has_many :memberships
   has_many :communities, through: :memberships
 
-  # validates :username, presence: true, uniqueness: {case_sensitive: false, message: " must be unique."}
-  # validates :password, presence: true
-  # validates :name, presence: true, uniqueness: {case_sensitive: false}
-  # validates :experience, presence: true
-  # validates :birthyear, presence: true
-  # validates :bio, presence: true, length: {minimum: 100}
-  # validates :height, presence: true 
-  # validates :weight, presence: true
-  # validates :origin, presence: true
-  # validates :preference, presence: true
-  # validates :profile_pic, presence: true
+  validates :username, uniqueness: {case_sensitive: false, message: " must be unique."}
   
 end
