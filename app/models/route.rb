35b51e019fe786017244636aba9f0e4092ce981f
Aymes <aysikh@MacBook-Pro.fios-router.home>
2020-09-28 23:08:26 -0400

    made changes to climbers form page, location form page, and route form page

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,9 @@ class Route < ApplicationRecord
   belongs_to :location
   has_many :projects
   has_many :climbers, through: :projects
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :difficulty, presence: true
+  validates :style, presence: true
+
 end
